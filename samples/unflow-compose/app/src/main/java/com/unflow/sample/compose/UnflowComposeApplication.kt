5f99f9656f30d31273f7444d62ccfb94fba05612
Kavi Dhokia <kavi@spectrl.com>
2022-03-28 12:13:39 +0100

    add analytics to compose sample

--- samples/unflow-compose/app/src/main/java/com/unflow/sample/compose/UnflowComposeApplication.kt
+++ samples/unflow-compose/app/src/main/java/com/unflow/sample/compose/UnflowComposeApplication.kt
@@ -1,6 +1,9 @@
 package com.unflow.sample.compose
 
 import android.app.Application
+import android.util.Log
+import com.unflow.analytics.AnalyticsListener
+import com.unflow.analytics.domain.model.UnflowEvent
 import com.unflow.androidsdk.UnflowSdk
 
 @Suppress("unused")
@@ -9,12 +12,20 @@ class UnflowComposeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val unflowAnalyticsListener = object : AnalyticsListener {
+            override fun onEvent(event: UnflowEvent) {
+                // Do something with the events. e.g. Send to your existing analytics tool
+                Log.d("Unflow-Analytics", "$event")
+            }
+        }
+
         val unflow = UnflowSdk.initialize(
             application = this,
             config = UnflowSdk.Config(
                 apiKey = "<YOUR API KEY HERE>",
                 enableLogging = false
-            )
+            ),
+            analyticsListener = unflowAnalyticsListener
         )
 
         unflow.sync()
