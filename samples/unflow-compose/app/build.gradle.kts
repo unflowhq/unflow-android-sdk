312113856e614c254b2ad96645a762e3425ac6a7
Kavi Dhokia <kavi@spectrl.com>
2022-03-28 17:18:46 +0100

    custom openers in compose

--- samples/unflow-compose/app/build.gradle.kts
+++ samples/unflow-compose/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
     implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("io.coil-kt:coil-compose:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
