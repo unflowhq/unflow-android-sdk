9cb3b8bf4ffeb3fe18867710916f13aeb2308c82
Kavi Dhokia <kavi@spectrl.com>
2022-03-29 12:10:18 +0100

    custom opener layout

---
+++ samples/unflow-sample/app/src/main/java/com/unflow/sample/CustomAdapter.kt
@@ -0,0 +1,61 @@
+package com.unflow.sample
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.unflow.androidsdk.UnflowSdk
+import com.unflow.androidsdk.ui.opener.OpenerData
+
+class CustomOpenerAdapter : RecyclerView.Adapter<CustomOpenerAdapter.ViewHolder>() {
+
+    private val items = mutableListOf<OpenerData>()
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView = view.findViewById(R.id.customOpenerItemImage)
+        val textView: TextView = view.findViewById(R.id.customOpenerItemText)
+        var screenId: Long? = null
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            view.setOnClickListener {
+                screenId?.let {
+                    UnflowSdk.client().openScreen(it)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(viewGroup.context)
+            .inflate(R.layout.item_custom_opener, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val opener = items[position]
+        viewHolder.image.load(opener.imageUrl)
+        viewHolder.textView.text = opener.title
+        viewHolder.screenId = opener.screenId
+    }
+
+    override fun getItemCount() = items.size
+
+    // Naive implementation for sample purposes only – you should do something smarter.
+    // e.g. Using ListAdapter with item diffing.
+    @SuppressLint("NotifyDataSetChanged")
+    fun setItems(openers: List<OpenerData>) {
+        items.clear()
+        items.addAll(openers)
+        notifyDataSetChanged()
+    }
+}
--- samples/unflow-sample/app/src/main/java/com/unflow/sample/MainActivity.kt
+++ samples/unflow-sample/app/src/main/java/com/unflow/sample/MainActivity.kt
@@ -2,10 +2,31 @@ package com.unflow.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.unflow.androidsdk.UnflowSdk
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // See layout for how to add an OpenerView
         setContentView(R.layout.activity_main)
+
+        // We can customise the layout of the openers completely – here we
+        // take just the first three and put them in a vertical list
+        val customOpenerAdapter = CustomOpenerAdapter()
+        findViewById<RecyclerView>(R.id.customOpenerLayout).apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = customOpenerAdapter
+        }
+        lifecycleScope.launch {
+            UnflowSdk.client().openers()
+                .collectLatest {
+                    customOpenerAdapter.setItems(it.take(3))
+                }
+        }
     }
 }
